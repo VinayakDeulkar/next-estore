4fd8201f34187667e405d0a221fb0b1d870964ee
vinayakdeulkar <vinayak.deulkar@neosoftmail.com>
2025-05-12 18:41:37 +0530

    changes leaflet

--- src/app/delivery-address/deliveryAddress.jsx
+++ src/app/delivery-address/deliveryAddress.jsx
@@ -469,27 +469,32 @@ const DeliveryAddress = () => {
           internationalDelivery.delivery_country_code.toUpperCase() === "KW" ||
           areaDetails.area_id !== "" ? (
             <>
-              {showMap ? (
-                <DeliveryMapContainer
-                  selectedArea={areaDetails?.area}
-                  markerPosition={markerPosition}
-                  setMarkerPosition={setMarkerPosition}
-                  selectedBounds={selectedBounds}
-                  setSelectedBounds={setSelectedBounds}
-                  triggerClick={handleMapLoad}
-                />
-                // <LeafletMap />
-              ) : (
-                <NewAddressForm
-                  areaDetails={areaDetails}
-                  blockValidation={blockValidation}
-                  streetValidation={streetValidation}
-                  houseValidation={houseValidation}
-                  addressNameValidation={addressNameValidation}
-                  errorState={errorState}
-                  setMarkerPosition={setMarkerPosition}
-                />
-              )}
+              {
+                /* showMap */ true ? (
+                  // <DeliveryMapContainer
+                  //   selectedArea={areaDetails?.area}
+                  //   markerPosition={markerPosition}
+                  //   setMarkerPosition={setMarkerPosition}
+                  //   selectedBounds={selectedBounds}
+                  //   setSelectedBounds={setSelectedBounds}
+                  //   triggerClick={handleMapLoad}
+                  // />
+                  <LeafletMap
+                    markerPosition={markerPosition}
+                    setMarkerPosition={setMarkerPosition}
+                  />
+                ) : (
+                  <NewAddressForm
+                    areaDetails={areaDetails}
+                    blockValidation={blockValidation}
+                    streetValidation={streetValidation}
+                    houseValidation={houseValidation}
+                    addressNameValidation={addressNameValidation}
+                    errorState={errorState}
+                    setMarkerPosition={setMarkerPosition}
+                  />
+                )
+              }
             </>
           ) : (
             <InternationalAddress internationalError={internationalError} />
--- src/components/LeafletMap/leafletMap.jsx
+++ src/components/LeafletMap/leafletMap.jsx
@@ -1,33 +1,46 @@
 import React from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import L from "leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
-const LeafletMap = () => {
-  delete L.Icon.Default.prototype._getIconUrl;
-  L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png").default,
-    iconUrl: require("leaflet/dist/images/marker-icon.png").default,
-    shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
+const DefaultIcon = L.icon({
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+function HandleMapClick({ setMarkerPosition }) {
+  useMapEvents({
+    click(e) {
+      setMarkerPosition([e.latlng.lat, e.latlng.lng]);
+    },
   });
+  return null;
+}
+
+const LeafletMap = ({ markerPosition, setMarkerPosition }) => {
+  console.log(markerPosition, "markerPosition");
 
   return (
     <MapContainer
       center={[51.505, -0.09]}
       zoom={13}
+      scrollWheelZoom={false}
       style={{ height: "500px", width: "100%" }}
     >
       <TileLayer
-        attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty popup.
-          <br />
-          Easily customizable.
-        </Popup>
-      </Marker>
+      <HandleMapClick setMarkerPosition={setMarkerPosition} />
+      {markerPosition && <Marker position={markerPosition}></Marker>}
     </MapContainer>
   );
 };
